851473afc3e6929055b65ab878f3e6afdcd56ead
AlexWasHeree <victoralexandre2303@gmail.com>
2022-05-16 12:55:09 -0300

    navbar complete

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg'
+import './styles.css'
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="moview-nav-content">
+                    <h1>Moview</h1>
+                    <a href="https://github.com/AlexWasHeree">
+                        <div className='moview-contact-container'>
+                            <GithubIcon />
+                            <p className='moview-contact-github'>/AlexWasHeree</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
